092c80a1bc18236469b9736667cb1adb4a957e1a
Chiara <cciriani@kovix-group.com>
2024-08-14 11:49:28 +0200

    feat: add different origins to script test domains probabilities

--- src/script_test_domains_probabilites.py
+++ src/script_test_domains_probabilites.py
@@ -14,45 +14,62 @@ num_nodes = 600
 tx_range = 5 
 max_distance = 5 
 
-all_results = []
+data_source_0 = []
+data_source_5 = []
+data_source_10 = []
 
 # Realiza múltiples simulaciones para recopilar datos
 for _ in range(200):
     results = calculate_probabilities(width, height, num_nodes, NUM_STREET_LIGHTS, tx_range, max_distance, False)
-    all_results.append(results)
-
-# Organiza los resultados en una estructura de datos adecuada
-data = []
-for results in all_results:
-    data.append({
-        'Edges removed domain': results['probability_mc_domain1'],
-        'Disjoint paths domain': results['probability_mc_domain3'],
-        'Common Neighbor domain': results['probability_mc_domain2']
+    
+    data_source_0.append({
+        'Edges removed domain': results['probability_mc_domain1_source_0'],
+        'Disjoint paths domain': results['probability_mc_domain3_source_0'],
+        'Common Neighbor domain': results['probability_mc_domain2_source_0']
+    })
+    
+    data_source_5.append({
+        'Edges removed domain': results['probability_mc_domain1_source_5'],
+        'Disjoint paths domain': results['probability_mc_domain3_source_5'],
+        'Common Neighbor domain': results['probability_mc_domain2_source_5']
+    })
+    
+    data_source_10.append({
+        'Edges removed domain': results['probability_mc_domain1_source_10'],
+        'Disjoint paths domain': results['probability_mc_domain3_source_10'],
+        'Common Neighbor domain': results['probability_mc_domain2_source_10']
     })
 
-df = pd.DataFrame(data)
-print(df)
+df_source_0 = pd.DataFrame(data_source_0)
+df_source_5 = pd.DataFrame(data_source_5)
+df_source_10 = pd.DataFrame(data_source_10)
 
-# Resumen estadístico
-summary = df.describe()
-print(summary)
-
-# Boxplot para comparar resultados de los enfoques
-plt.figure(figsize=(12, 8))
-sns.boxplot(data=df)
-plt.title('Comparison of Probabilities for Different Domains')
-plt.show()
-
-# CDF plot for each approach
+# Graficar el CDF para cada origen (SL 0, SL 5, SL 10)
 approaches = ['Edges removed domain', 'Disjoint paths domain', 'Common Neighbor domain']
 
-# Graficar el CDF para cada dominio
 plt.figure(figsize=(12, 8))
 for approach in approaches:
-    sns.ecdfplot(data=df, x=approach, label=approach)
-
+    sns.ecdfplot(data=df_source_0, x=approach, label=approach)
 plt.title('CDF in a network of 600 nodes and 11 street lights')
-plt.xlabel('Probability that all the street lights receive message from SL 0')
+plt.xlabel('Probability that all street lights receive message from SL 0')
+plt.ylabel('Cumulative Probability')
+plt.legend(title='Domain')
+plt.show()
+
+plt.figure(figsize=(12, 8))
+for approach in approaches:
+    sns.ecdfplot(data=df_source_5, x=approach, label=approach)
+plt.title('CDF in a network of 600 nodes and 11 street lights')
+plt.xlabel('Probability that all street lights receive message from SL 5')
+plt.ylabel('Cumulative Probability')
+plt.legend(title='Domain')
+plt.show()
+
+plt.figure(figsize=(12, 8))
+for approach in approaches:
+    sns.ecdfplot(data=df_source_10, x=approach, label=approach)
+plt.title('CDF in a network of 600 nodes and 11 street lights')
+plt.xlabel('Probability that all street lights receive message from SL 10')
 plt.ylabel('Cumulative Probability')
 plt.legend(title='Domain')
 plt.show()
